4bc6f14a075d2123add6531244c0e1bb6bea6d81
Rokas <rookas.rudzenskas@gmail.com>
2021-05-02 17:56:08 +0300

    Rokas

--- script.js
+++ script.js
@@ -1,50 +1,61 @@
 let counter1 = 0;
 let counter2 = 1;
+let bool = true;
 
-const sections = document.querySelectorAll('section');
-const progress = document.querySelector('.progressh2');
+const sections = document.querySelectorAll("section");
+const progress = document.querySelector(".progress h2");
+const circles = document.querySelectorAll(".circle");
+const menu = document.querySelector(".menu");
+const section1wrapper = document.querySelector(".section-1-wrapper");
+const section5wrapper = document.querySelector(".section-5-wrapper");
 
-const circles = document.querySelectorAll('.circle');
-
-const pageController = () => {
-  if(counter1 === 5) {
-    Array.from(sections).forEach((section) => {
-      section.style.left = '0'
-    })
-    counter1 = 0;
-    counter2 = 1;
-    progressCounter();
-    return;
-  }
-
-  if(counter1 === -1) {
-    Array.from(sections).forEach(section => {
-      if(section.classList[0] === 'section-5') {
-        return;
-      }
-      section.style.left = "-100vw";
-    })
-    counter1 = 4;
-    counter2 = 5;
-    progressCounter();
-  }
-progressCounter();
-}
+section1wrapper.style.transform = "scale(1)";
 
 const progressCounter = () => {
   progress.textContent = `${counter2}/${sections.length}`;
 
-  Array.from(circles).forEach(circle => {
-    circle.style.backgroundColor = 'transparent';
+  Array.from(circles).forEach((circle) => {
+    circle.style.backgroundColor = "transparent";
   });
-  document.querySelector(`.circle-${counter2}`).style.backgroundColor = "ddd";
+  document.querySelector(`.circle-${counter2}`).style.backgroundColor = "#ddd";
 };
 
-window.addEventListener('wheel', (e) => {
+const pageController = () => {
+  bool = true;
+  if (counter1 === 5) {
+    Array.from(sections).forEach((section) => {
+      section.style.left = "0";
+    });
+    counter1 = 0;
+    counter2 = 1;
+    section1wrapper.style.transform = "scale(1)";
+    section5wrapper.style.transform = "scale(1.5)";
+    progressCounter();
+    bool = false;
+  }
 
+  if (counter1 === -1) {
+    Array.from(sections).forEach((section) => {
+      if (section.classList[0] === "section-5") {
+        return;
+      }
+      section.style.left = "-100vw";
+    });
+    counter1 = 4;
+    counter2 = 5;
+    section1wrapper.style.transform = "scale(1.5)";
+    section5wrapper.style.transform = "scale(1)";
+    progressCounter();
+    bool = false;
+  }
+  progressCounter();
+  return bool;
+};
+
+window.addEventListener("wheel", (e) => {
   const deltaY = e.deltaY > 0;
 
-  if(deltaY) {
+  if (deltaY) {
     counter1++;
     counter2++;
   } else {
@@ -52,44 +63,61 @@ window.addEventListener('wheel', (e) => {
     counter2--;
   }
 
-  if(counter1 === 5) {
-    Array.from(sections).forEach((section) => {
-      section.style.left = '0'
-    })
-    counter1 = 0;
-    counter2 = 1;
-    progressCounter();
-    return;
-  }
-
-  if(counter1 === -1) {
-    Array.from(sections).forEach(section => {
-      if(section.classList[0] === 'section-5') {
-        return;
-      }
-      section.style.left = "-100vw";
-    })
-    counter1 = 4;
-    counter2 = 5;
-    progressCounter();
-  }
-
+  pageController();
   progressCounter();
+  console.log(counter1, counter2);
 
-  document.querySelector(`.section-${deltaY ? counter1 : counter2}`).style.left = `${deltaY ? "-100vw" : "0"}`;
+  if (bool) {
+    document.querySelector(
+      `.section-${deltaY ? counter1 : counter2}`
+    ).style.left = `${deltaY ? "-100vw" : "0"}`;
 
-  console.log(counter1, counter2s);
+    document.querySelector(
+      `.section-${deltaY ? counter1 : counter2}-wrapper`
+    ).style.transform = `scale(${deltaY ? "1.5" : "1"})`;
+
+    document.querySelector(
+      `.section-${deltaY ? counter1 + 1 : counter2 + 1}-wrapper`
+    ).style.transform = `scale(${deltaY ? "1" : "1.5"})`;
+  }
 });
 
-
-document.querySelector('.left-btn').addEventListener('click', () => {
+document.querySelector(".left-btn").addEventListener("click", () => {
   counter1--;
   counter2--;
-  document.querySelector(`.section-${counter2}`).style.left = '0';
+  pageController() &&
+    (document.querySelector(`.section-${counter2}`).style.left = "0");
+
+  if (bool) {
+    document.querySelector(`.section-${counter2}-wrapper`).style.transform =
+      "scale(1)";
+    document.querySelector(`.section-${counter2 + 1}-wrapper`).style.transform =
+      "scale(1.5)";
+  }
 });
 
-document.querySelector('.right-btn').addEventListener('click', () => {
+document.querySelector(".right-btn").addEventListener("click", () => {
   counter1++;
   counter2++;
-  document.querySelector(`.section-${counter1}`).style.left = '-100vw';
+  pageController() &&
+    (document.querySelector(`.section-${counter1}`).style.left = "-100vw");
+
+  if (bool) {
+    document.querySelector(`.section-${counter2}-wrapper`).style.transform =
+      "scale(1)";
+    document.querySelector(`.section-${counter1}-wrapper`).style.transform =
+      "scale(1.5)";
+  }
+});
+
+document.querySelector(".grapes-img").addEventListener("mouseover", () => {
+  document.querySelector(".section-3-wrapper").style.opacity = ".5";
+});
+
+document.querySelector(".grapes-img").addEventListener("mouseout", () => {
+  document.querySelector(".section-3-wrapper").style.opacity = "1";
+});
+
+menu.addEventListener("click", () => {
+  document.querySelector(".navbar").classList.toggle("change");
 });
